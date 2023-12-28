1706ca775cc449b7b0c018ceb34c85e2e40a4381
The Financier <itisenward@gmail.com>
2023-12-29 01:59:36 +0600

    Initial commmit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'fronpage',
+        type: 'postgresql',
+        debug: !__prod__
+    })
+}
+
+main()
